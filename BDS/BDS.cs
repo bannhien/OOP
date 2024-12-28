3be19a8614fd6da698c9de0eea22a57062e11d1e
bannhien <126856527+bannhien@users.noreply.github.com>
2024-12-28 14:31:09 +0700

    Add files via upload

---
+++ BDS/BDS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String input = File.ReadAllText(@"test.txt");
+        int i =0;
+        int j = 0;
+        Dictionary<int, string> sheet = new Dictionary<int, string>();
+        foreach (var row in input.Split('\n'))
+        {
+            j = 0;
+            foreach (var col in row.Trim().Split(','))
+            {
+                sheet[i*10+j] = col;
+                Console.WriteLine(sheet[i*10+j]);
+                j++;
+            }
+            i++;
+        }
+    }
+}
